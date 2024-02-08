fae37fe757f2a188ccc2c8cc964402869d5b973b
MikaIsaak <michaelisayan@gmail.com>
2024-02-08 10:07:33 +0100

    Fixed

---
+++ contracts/ERC20/USDT.sol
@@ -0,0 +1,11 @@
+// contracts/GLDToken.sol
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract USDT is ERC20 {
+    constructor(uint amount) ERC20("USDT", "USDT") {
+        _mint(msg.sender, amount);
+    }
+}
